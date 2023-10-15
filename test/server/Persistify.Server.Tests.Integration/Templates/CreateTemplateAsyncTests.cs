071bfd3de632b33e62bca7289c15d6000a976648
Łukasz Sobczak <81999946+sobczal2@users.noreply.github.com>
2023-10-15 16:21:20 +0200

    PS-105 wip

---
+++ test/server/Persistify.Server.Tests.Integration/Templates/CreateTemplateAsyncTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Persistify.Domain.Templates;
+using Persistify.Requests.Templates;
+using Persistify.Server.Tests.Integration.Common;
+using ProtoBuf.Grpc;
+using Xunit;
+
+namespace Persistify.Server.Tests.Integration.Templates;
+
+public class CreateTemplateAsyncTests : IntegrationTestBase
+{
+    [Fact]
+    public async Task CreateTemplateAsync_WhenCorrect_ReturnsOk()
+    {
+        // Arrange
+        var callContext = await GetAuthorizedCallContextAsRootAsync();
+        var request = new CreateTemplateRequest
+        {
+            TemplateName = "TestTemplate",
+            TextFields = new List<TextField>
+            {
+                new()
+                {
+                    Name = "TextField1",
+                    Required = true,
+                    AnalyzerDescriptor = new PresetAnalyzerDescriptor { PresetName = "standard" }
+                }
+            },
+        };
+
+        // Act
+        var response = await TemplateService.CreateTemplateAsync(request, callContext);
+
+        // Assert
+        response.Should().NotBeNull();
+    }
+}
---
+++ test/server/Persistify.Server.Tests.Integration/Templates/ListTemplatesAsyncTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Persistify.Domain.Templates;
+using Persistify.Requests.Shared;
+using Persistify.Requests.Templates;
+using Persistify.Server.Tests.Integration.Common;
+using Xunit;
+
+namespace Persistify.Server.Tests.Integration.Templates;
+
+public class ListTemplatesAsyncTests : IntegrationTestBase
+{
+    [Fact]
+    public async Task ListTemplatesAsync_WhenCorrect_ReturnsOk()
+    {
+        // Arrange
+        var callContext = await GetAuthorizedCallContextAsRootAsync();
+        var addTemplateRequest = new CreateTemplateRequest
+        {
+            TemplateName = "TestTemplate",
+            TextFields = new List<TextField>
+            {
+                new()
+                {
+                    Name = "TextField1",
+                    Required = true,
+                    AnalyzerDescriptor = new PresetAnalyzerDescriptor { PresetName = "standard" }
+                }
+            },
+        };
+
+        await TemplateService.CreateTemplateAsync(addTemplateRequest, callContext);
+        var request = new ListTemplatesRequest { Pagination = new Pagination { PageNumber = 0, PageSize = 10 } };
+
+        // Act
+        var response = await TemplateService.ListTemplatesAsync(request, callContext);
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Templates.Should().NotBeNull();
+        response.Templates.Should().HaveCount(1);
+        response.Templates.First().Name.Should().Be("TestTemplate");
+        response.Templates.First().TextFields.Should().NotBeNull();
+        response.Templates.First().TextFields.Should().HaveCount(1);
+        response.Templates.First().TextFields[0].Name.Should().Be("TextField1");
+        response.Templates.First().TextFields[0].Required.Should().BeTrue();
+        response.Templates.First().TextFields[0].AnalyzerDescriptor.Should().NotBeNull();
+        response.Templates.First().TextFields[0].AnalyzerDescriptor.Should().BeOfType<PresetAnalyzerDescriptor>();
+        ((PresetAnalyzerDescriptor)response.Templates.First().TextFields[0].AnalyzerDescriptor).PresetName.Should().Be("standard");
+    }
+}
