dd30562e5d74891fd41699f126009aaf822d3d17
sobczal2 <81999946+sobczal2@users.noreply.github.com>
2023-12-05 17:12:12 +0100

    add run gc endpoint

---
+++ src/server/Persistify.Server/Services/UtilService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Persistify.Requests.Util;
+using Persistify.Responses.Util;
+using Persistify.Services;
+using ProtoBuf.Grpc;
+
+namespace Persistify.Server.Services;
+
+public class UtilService : IUtilService
+{
+    public ValueTask<RunGcResponse> RunGcAsync(RunGcRequest request, CallContext callContext)
+    {
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+        return new ValueTask<RunGcResponse>(new RunGcResponse());
+    }
+}
